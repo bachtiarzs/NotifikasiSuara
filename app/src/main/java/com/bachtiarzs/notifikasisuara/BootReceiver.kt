4a46b3c25a53809c28047939d94df6e37e97cb9e
bachtiarzs <125351037+bachtiarzs@users.noreply.github.com>
2026-03-31 22:17:13 +0700

    Create BootReceiver.kt

---
+++ app/src/main/java/com/bachtiarzs/notifikasisuara/BootReceiver.kt
@@ -0,0 +1,14 @@
+package com.bachtiarzs.notifikasisuara
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("BootReceiver", "Device booted, service akan aktif otomatis via NotificationListenerService")
+        }
+    }
+}
